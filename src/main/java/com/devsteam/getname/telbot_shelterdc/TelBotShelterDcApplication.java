cff2b4ea4f1f2a13d06aab80d2ef64b5af62004c
afevralevtotyt <afevraleff@ya.ru>
2023-04-12 18:20:58 +0200

    branches

--- src/main/java/com/devsteam/getname/telbot_shelterdc/TelBotShelterDcApplication.java
+++ src/main/java/com/devsteam/getname/telbot_shelterdc/TelBotShelterDcApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TelBotShelterDcApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TelBotShelterDcApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TelBotShelterDcApplication.class, args);
+    }
 
 }
