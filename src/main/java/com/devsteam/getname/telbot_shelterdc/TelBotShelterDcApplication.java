15759fff8da9a1fc64beddb45065f2683a10a01d
afevralevtotyt <afevraleff@ya.ru>
2023-04-12 18:11:56 +0200

    Первый коммит, ветка master

---
+++ src/main/java/com/devsteam/getname/telbot_shelterdc/TelBotShelterDcApplication.java
@@ -0,0 +1,13 @@
+package com.devsteam.getname.telbot_shelterdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelBotShelterDcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelBotShelterDcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsteam/getname/telbot_shelterdc/TelBotShelterDcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsteam.getname.telbot_shelterdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelBotShelterDcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
