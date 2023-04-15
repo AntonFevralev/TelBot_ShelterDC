665ce7ba816fe673c0265b2f478d2d8d2db133ab
rus <ruslan.cheremisin@gmail.com>
2023-04-15 14:18:32 +0200

    Отредактирован класс Report Отредактирован класс ReportService Создано исключение пустого списка report

--- src/main/java/com/devsteam/getname/telbot_shelterdc/model/Report.java
+++ src/main/java/com/devsteam/getname/telbot_shelterdc/model/Report.java
@@ -1,6 +1,7 @@
 package com.devsteam.getname.telbot_shelterdc.model;
 
 import java.io.File;
+import java.time.LocalDateTime;
 
 public class Report {
 
@@ -10,11 +11,38 @@ public class Report {
     private final String wellBeingAndAdaptation;
     private final String behaviorChanges;
 
-    public Report(long id, File photo, String meals, String wellBeingAndAdaptation, String behaviorChanges) {
+    private final LocalDateTime reportDateTime;
+
+    public Report(long id, File photo, String meals, String wellBeingAndAdaptation, String behaviorChanges, LocalDateTime localDateTime) {
         this.id = id;
         this.photo = photo;
         this.meals = meals;
         this.wellBeingAndAdaptation = wellBeingAndAdaptation;
         this.behaviorChanges = behaviorChanges;
+        this.reportDateTime = localDateTime;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public File getPhoto() {
+        return photo;
+    }
+
+    public String getMeals() {
+        return meals;
+    }
+
+    public String getWellBeingAndAdaptation() {
+        return wellBeingAndAdaptation;
+    }
+
+    public String getBehaviorChanges() {
+        return behaviorChanges;
+    }
+
+    public LocalDateTime getReportDateTime() {
+        return reportDateTime;
     }
 }
