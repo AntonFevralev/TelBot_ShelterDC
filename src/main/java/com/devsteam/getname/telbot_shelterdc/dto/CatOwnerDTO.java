746f552997d23507364e31f077601928b407289b
DmitryRubtsov53 <2578390@mail.ru>
2023-04-25 16:48:07 +0500

    Добавил в DTO id владельца.

--- src/main/java/com/devsteam/getname/telbot_shelterdc/dto/CatOwnerDTO.java
+++ src/main/java/com/devsteam/getname/telbot_shelterdc/dto/CatOwnerDTO.java
@@ -3,12 +3,12 @@ package com.devsteam.getname.telbot_shelterdc.dto;
 import com.devsteam.getname.telbot_shelterdc.model.CatOwner;
 import com.devsteam.getname.telbot_shelterdc.model.StatusOwner;
 
-public record CatOwnerDTO(Long chatId, String fullName, String phone, String address,
+public record CatOwnerDTO(Long idCO, Long chatId, String fullName, String phone, String address,
                           StatusOwner statusOwner) {
 
 
     public static CatOwnerDTO catOwnerToDTO(CatOwner catOwner){
-        return new CatOwnerDTO(catOwner.getChatId(), catOwner.getFullName(), catOwner.getPhone(),
+        return new CatOwnerDTO(catOwner.getIdCO(), catOwner.getChatId(), catOwner.getFullName(), catOwner.getPhone(),
                 catOwner.getAddress(), catOwner.getStatusOwner());
 
     }
