0e8a54b34bb13a31a2c28fe5de827e7e603c0724
rus <ruslan.cheremisin@gmail.com>
2023-05-07 17:10:43 +1000

    написал тесты на сервис, всё работает Начал писать тесты на контроллер,

--- src/test/java/com/devsteam/getname/telbot_shelterdc/controller/ReportControllerTest.java
+++ src/test/java/com/devsteam/getname/telbot_shelterdc/controller/ReportControllerTest.java
@@ -36,7 +36,7 @@ public class ReportControllerTest {
 
     @Test
     void givenNoReportInDatabase_whenGetReports_thenNoReportsAreFound() throws Exception {
-        mockMvc.perform(get("http://localhost:8080/pets/report?kind=DOG"))
+        mockMvc.perform(get("http://localhost:8080/pets/report?kind=CAT"))
                 .andExpect(status().is5xxServerError());
     }
 
