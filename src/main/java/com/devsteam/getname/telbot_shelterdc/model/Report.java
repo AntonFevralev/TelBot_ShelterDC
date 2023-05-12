c72e5d7e0d805d9b22be82d9af63e7f86da80a45
afevralevtotyt <afevralevtotyt@gmail.com>
2023-05-12 13:42:00 +0200

    изменил размер поля описания животного

--- src/main/java/com/devsteam/getname/telbot_shelterdc/model/Report.java
+++ src/main/java/com/devsteam/getname/telbot_shelterdc/model/Report.java
@@ -28,7 +28,7 @@ public class Report {
     /**Поле владельца животного, который пишет отчёт*/
     private PetOwner petOwner;
 
-    @Column(length = 5000)
+    @Column(length = 1024)
     /**Поле с описанием рациона и режима питания, адаптации и состояния, а также изменений в поведении животного*/
     private String mealsWellBeingAndAdaptationBehaviorChanges;
     @Column()
